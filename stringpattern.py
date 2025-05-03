a3154c908acc04b49edd0d1805e13ecf628ff3cc
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2025-05-03 14:05:10 +0530

    Add files via upload

---
+++ stringpattern.py
@@ -0,0 +1,8 @@
+name = "Dua Lipa"
+print(name[0]*4,"",name[4])
+print(name[0]," ",name[0], name[4])
+print(name[0]," ",name[0], name[4])
+print(name[0]," ",name[0], name[4])
+print(name[0]," ",name[0], name[4])
+print(name[0]," ",name[0], name[4])
+print(name[0]*4,"",name[4]*4)
